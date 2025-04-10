19d6fb725f13e27c5a9345423ebc1a4885c1063e
Lautar0 <104235333+Saturno0@users.noreply.github.com>
2025-04-10 12:44:07 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Home from "./pages/Home";
 import Product from "./components/Product.jsx";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Checkout from "./components/Checkout";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/product/:id" element={<Product/>} />
+      <Route path="/carrito" element={<Checkout/>} />
     </Routes>
   );
 }
