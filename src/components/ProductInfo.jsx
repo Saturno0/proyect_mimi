5fdf61b5dda270b145b68ea825e340aaab9154ac
Lautaro <lautaro.agustint@gmail.com>
2025-07-13 22:37:09 -0300

    arregle los datos de los productos

--- src/components/ProductInfo.jsx
+++ src/components/ProductInfo.jsx
@@ -1,13 +1,13 @@
-
+import PropTypes from 'prop-types';
 
 const ProductInfo = ({ product }) => {
     return (
         <>
-            <h2>{product.nombre}</h2>
+            <h2>{product.name}</h2>
             <div className="product-rating">
                 ⭐⭐⭐⭐☆ ({product.calificacion}) · {product.opiniones} opiniones
             </div>
-            <p>{product.descripcion}</p>
+            <p>{product.description}</p>
 
             {product.stock ? <p className="stock">En stock</p> : <p className="stock">Sin stock</p>}
             {product.descuento > 0 && (
@@ -36,5 +36,36 @@ const ProductInfo = ({ product }) => {
         </>
     )
 }
+
+ProductInfo.propTypes = {
+  product: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      name: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      calificacion: PropTypes.number.isRequired,
+      opiniones: PropTypes.number.isRequired,
+      stock: PropTypes.bool.isRequired,
+      descuento: PropTypes.number.isRequired,
+      precio_actual: PropTypes.number.isRequired,
+      precio_original: PropTypes.number.isRequired,
+      tamaños: PropTypes.arrayOf(PropTypes.string).isRequired,
+      especificaciones: PropTypes.shape({
+        material: PropTypes.string.isRequired,
+        peso: PropTypes.string.isRequired,
+        fabricado_en: PropTypes.string.isRequired
+      }).isRequired,
+      colores: PropTypes.arrayOf(
+        PropTypes.shape({
+          nombre: PropTypes.string.isRequired,
+          cantidad: PropTypes.number.isRequired,
+          stock: PropTypes.number.isRequired
+        })
+      ).isRequired,
+      ingreso: PropTypes.string.isRequired
+    }).isRequired
+};
 
 export default ProductInfo;
